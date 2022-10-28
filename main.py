3519e5dad52a9384e4bb37919e655be148427f12
juseok <juseok.lee@monitorapp.com>
2022-10-28 15:26:15 +0900

    end

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from xml.dom.minidom import Element
 from sqlalchemy import create_engine
 import pymysql
 import numpy as np
@@ -8,7 +9,12 @@ import re
 from scapy.all import *
 from scapy.layers import http
 import sqlalchemy
-
+import requests
+from bs4 import BeautifulSoup as bs
+import multiprocessing as mp
+import argparse
+from warnings import filterwarnings
+filterwarnings("ignore")
 
 class BP_CVE(PyExploitDb):
     def __init__(self):
@@ -17,7 +23,6 @@ class BP_CVE(PyExploitDb):
         self.debug = False
         self.pcap_packet = {}
         self.openFile()
-        
         self.read_pcap()
         
     def read_pcap(self):
@@ -70,8 +75,6 @@ class BP_CVE(PyExploitDb):
         excel_df = excel_df.drop(columns=['Strike Result', 'Permutations'])
         # rename col
         excel_df = excel_df.rename(columns={'Time of strike':'Time', 'Strike Name':'Name', 'Strike Reference':'Reference', 'Strike Tuples':'Network'})
-        #edit Name
-        excel_df['Name'] = excel_df['Name'].apply(lambda x: x.split('(')[0][:-1])
         self.bp_df = excel_df.sort_values('Time').reset_index(drop=True)
         #print(self.bp_df) #debug
         return
@@ -98,7 +101,7 @@ class BP_CVE(PyExploitDb):
             if len(result) != 0:
                 filename = result['exploit']
                 
-        if len(filename) != 0:
+        if len(filename) > 5:
             filename = filename.replace('\\', '\\\\')
             with open(filename, 'r', encoding='UTF8') as f:
                 exploit = f.read()
@@ -140,6 +143,8 @@ class BP_CVE(PyExploitDb):
                     temp = str(pkt['HTTPRequest'])[2:-1].replace('\\r\\n', '\n')
                     #print(temp)
                     result += temp+'\n----------------------------------\n'
+        if len(result) == 0:
+            return np.NaN
         return str(result)
 
     def merge_df(self, ex, net):
@@ -149,7 +154,8 @@ class BP_CVE(PyExploitDb):
         return
 
     def print_excel(self, path):
-        self.bp_df.to_excel(
+        excel_df = self.bp_df.drop(columns='Exploit')
+        excel_df.to_excel(
             path,
             engine='xlsxwriter'
         )
@@ -164,25 +170,124 @@ class BP_CVE(PyExploitDb):
             'Reference':sqlalchemy.types.TEXT,
             'Network':sqlalchemy.types.VARCHAR(255),
             'Exploit':sqlalchemy.dialects.mysql.MEDIUMTEXT,
-            'Net_Exploit':sqlalchemy.dialects.mysql.MEDIUMTEXT
+            'Net_Exploit':sqlalchemy.dialects.mysql.MEDIUMTEXT,
+            'CVSS':sqlalchemy.types.DECIMAL(3,1),
+            'Priority':sqlalchemy.types.INT
         }
-        self.bp_df.to_sql(name='BP_CVE', con=db_connection, if_exists='replace', index=False, dtype=dtypesql)
+        out_df = self.bp_df  
+        out_df['Name'] = out_df['Name'].apply(lambda x: x.split('(')[0][:-1])
+        out_df.to_sql(name='BP_CVE', con=db_connection, if_exists='replace', index=False, dtype=dtypesql)
         conn.execute(f"ALTER TABLE BP_CVE ADD PRIMARY KEY(Time);")
         conn.close()
         
         return
         
+    def select_multiprocessing(self, n_cores=8):
+        cvss_df = self.bp_df
+        df_split = np.array_split(cvss_df, n_cores)
+        pool = mp.Pool(n_cores)
+        cvss_df = pd.concat(pool.map(self.apply_cvss, df_split))
+        pool.close()
+        pool.join()
+        self.bp_df = cvss_df
+        return
     
-def main():
+    def apply_cvss(self, df):
+        df['CVSS'] = df.apply(lambda row: self.parse_cvss(row['Name']), axis=1)
+        return df
+    
+    def parse_cvss(self, name):
+        url = 'https://' + name.split('(https://')[1][:-1]
+        page = requests.get(url)
+        soup = bs(page.text, "lxml")
+        cvss = soup.find('div', class_='field-name-field-cvss')
+        if cvss != None:
+            return float(cvss.text.split()[0])
+        else : 
+            return np.NaN
+            
+    def prioritize(self):
+        priority_df = self.bp_df
+        priority_df['Priority'] = priority_df.apply(lambda row: self.apply_priority(row),axis=1)
+        priority_df['CVE'] = priority_df['Reference'].apply(lambda x: self.select_cve(x))
+        priority_df = priority_df.sort_values(by=['Priority', 'CVSS', 'CVE'], ascending=[True, False, True]).drop(columns='CVE').reset_index(drop=True)
+        self.bp_df = priority_df
+        return
+    
+    def apply_priority(self, x):
+        ex = x['Exploit']
+        net = x['Net_Exploit']
+        cvss = x['CVSS']
+        
+        result = 4
+        if ex != ex and net != net:
+            result = 4
+        elif (ex == ex and net != net) or (ex != ex and net == net):
+            result = 3
+        elif ex == ex and net == net and (cvss != cvss or cvss < 7):
+            result = 2
+        elif ex == ex and net == net and cvss == cvss and cvss > 7:
+            result = 1
+        return result
+    
+    def select_cve(self, x):
+        if x == x and 'CVE' in x:
+            return x.split('CVE')[1].split()[0]
+        else :
+            return np.NaN
+        
+    
+    
+def sample():
     bp_cve = BP_CVE()
-    #test
     bp_cve.read_Excel('data/TA-BP-CVE-TEST-attack_report-2022.07-arrange-jk.xlsx', 'arrange-jk')
+    
+    print('select exploit..')
     ex_df = bp_cve.select_exploitdb()
     net_df = bp_cve.select_pcap()
+    print('merge dataframe..')
     bp_cve.merge_df(ex_df, net_df)
+    print('get CVSS...')
+    bp_cve.select_multiprocessing()
+    print('prioritize...')
+    bp_cve.prioritize()
+    print('make excel...')
+    bp_cve.print_excel('data/result.xlsx')
+    print('write DB...')
     bp_cve.save_db('bp', '4523','127.0.0.1:3306','bp_cve')
-    #product
+    
+def main():
+    parser = argparse.ArgumentParser(description='BP-CVE Data Collection Automation Tool')
+    parser.add_argument('-f', dest='bpcveexcel',help='read BP-CVE xlsx file path', required=True)
+    parser.add_argument('--sheet', dest='bpcveexcelsheet',help='read BP-CVE xlsx file sheet name',required=True)
+    parser.add_argument('-o', '--output', dest='output', help='outfile path', default='data/result.xlsx', required=False)
+    parser.add_argument('--db-user', dest='dbuser', help='MySQL DataBase User name', required=True)
+    parser.add_argument('--db-passwd', dest='dbpasswd', help='MySQL DataBase Password', required=True)
+    parser.add_argument('--db-host', dest='dbhost', help='MySQL DataBase Host(:port)', required=True)
+    parser.add_argument('--db', dest='db', help='MySQL DataBase', required=True)
+    
+    args = parser.parse_args()
+    #print(args)
+    
+    bp_cve = BP_CVE()
+    bp_cve.read_Excel(args.bpcveexcel, args.bpcveexcelsheet)
+    
+    print('select exploit..')
+    ex_df = bp_cve.select_exploitdb()
+    net_df = bp_cve.select_pcap()
+    print('merge dataframe..')
+    bp_cve.merge_df(ex_df, net_df)
+    print('get CVSS...')
+    bp_cve.select_multiprocessing()
+    print('prioritize...')
+    bp_cve.prioritize()
+    print('make excel...')
+    bp_cve.print_excel(args.output)
+    print('write DB...')
+    bp_cve.save_db(args.dbuser, args.dbpasswd, args.dbhost, args.db)
+    
     return
 
 if __name__ == "__main__":
-    main()
+    sample()
+    #main()
