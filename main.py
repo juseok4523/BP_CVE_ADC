92e1099748a45e8fbea3727ccd5b00ea3de9743c
juseok <juseok.lee@monitorapp.com>
2022-11-17 14:28:28 +0900

    save compare log in .txt file

--- main.py
+++ main.py
@@ -372,9 +372,12 @@ class BP_CVE(PyExploitDb):
             git_eq_s = ~git_bp_s.eq(git_db_s, fill_value=0)
             db_eq_df = db_df[git_eq_s].loc[:,['Id', 'Github_PoC']]
             bp_eq_df = temp_bp_df[git_eq_s].loc[:,['Id', 'Github_PoC']]
-            compare_df = pd.merge(db_eq_df, bp_eq_df, how="outer", on='Id')
-            compare_df = compare_df.rename(columns={'Github_PoC_x':'Before', 'Github_PoC_y':'After'})
+            compare_df = pd.merge(db_eq_df, bp_eq_df, how="outer", on='Id').rename(columns={'Github_PoC_x':'Before', 'Github_PoC_y':'After'})
+            with open('data/compare_log.txt', 'a') as file:
+                now = datetime.now()
+                compare_df.apply(lambda row: file.write(f"Update {now.strftime('%Y-%m-%d_%H_%M_%S')} : Before \"{row['Before']}\" -> After \"{row['After']}\"\n"), axis=1)
             print(compare_df)
+                
         else :
             print('Not Add Github_PoC')
         return
